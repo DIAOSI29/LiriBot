8066a99fccfef64778741637e5165113a0047188
Tian Qin <qintian29@gmail.com>
2019-11-24 18:50:50 +0800

    sportify functioning done with artist's name, song's name, spotify preview link and album name retrieved as well as displayed upon node input

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
-
 var axios = require("axios");
 var fs = require("fs");
 var keys = require("./keys.js");
 var moment = require("moment");
+var Spotify = require("node-spotify-api");
+
 var spotify = new Spotify(keys.spotify);
+
 var input1 = process.argv[2];
 var input2 = process.argv.slice(3).join(" ");
 var userInput = input1 + "," + input2;
@@ -39,6 +41,7 @@ fs.readFile("./random.txt", "utf-8", function(err, data) {
       axios
         .get(concertQueryUrl)
         .then(function(response) {
+          //   console.log(response);
           //   console.log(response.data[0].venue);
           //   console.log(response.data[0].venue.name);
           let concertResponse =
@@ -68,5 +71,54 @@ fs.readFile("./random.txt", "utf-8", function(err, data) {
         });
 
     case "spotify-this-song":
+      if (content == "") {
+        spotify
+          .search({
+            type: "track",
+            query: "ace of base",
+            limit: 1
+          })
+
+          .then(function(response) {
+            console.log(
+              "Artist's Name: " +
+                response.tracks.items[0].artists[0].name +
+                "\nSong's Name: " +
+                response.tracks.items[0].name +
+                "\nPreview Link: " +
+                response.tracks.items[0].external_urls.spotify +
+                "\nAlbum: " +
+                response.tracks.items[0].album.name +
+                "\n"
+            );
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      } else {
+        spotify
+          .search({ type: "track", query: content, limit: 5 })
+          .then(function(response) {
+            for (let i = 0; i < response.tracks.items.length; i++) {
+              let eachSearch = response.tracks.items[i];
+              // console.log(response.tracks.items[i]);
+              let spotifyDisplayResult =
+                "Artist's Name: " +
+                eachSearch.artists[0].name +
+                "\nSong's Name: " +
+                eachSearch.name +
+                "\nPreview Link: " +
+                eachSearch.external_urls.spotify +
+                "\nAlbum: " +
+                eachSearch.album.name +
+                "\n";
+
+              console.log(spotifyDisplayResult);
+            }
+          })
+          .catch(function(err) {
+            console.log(err);
+          });
+      }
   }
 });
